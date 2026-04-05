3e316498742b370ffce4bcb856118290148e2ad9
TarasNovik <148384148+TarasNovik@users.noreply.github.com>
2026-04-05 09:41:12 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, preview } from 'vite';
+import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
