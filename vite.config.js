8d06ef74e60b0227c257d31545192541eb0ca553
TarasNovik <148384148+TarasNovik@users.noreply.github.com>
2026-04-05 09:49:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,32 @@ import { defineConfig, preview } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-export default defineConfig({ base: '/' });
+
+export default defineConfig(({ command }) => {
+    return {
+        base: '/equiblick1/',
+        define: {
+            [command === 'serve' ? 'global' : '_global']: {},
+        },
+
+        root: 'src',
+        build: {
+            sourcemap: true,
+
+            rollupOptions: {
+                input: glob.sync('./src/*.html'),
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return 'vendor';
+                        }
+                    },
+                    entryFileNames: 'commonHelpers.js',
+                },
+            },
+            outDir: '../dist',
+        },
+        plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    };
+});
 
