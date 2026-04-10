de03bff038568badb2304ff6310b23310675a605
TarasNovik <148384148+TarasNovik@users.noreply.github.com>
2026-04-10 21:55:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,7 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-    return {
-        base: '/',
-        define: {
-            [command === 'serve' ? 'global' : '_global']: {},
-        },
+import { defineConfig } from 'vite'
 
-        root: 'src',
-        build: {
-            sourcemap: true,
-
-            rollupOptions: {
-                input: glob.sync('./src/*.html'),
-                output: {
-                    manualChunks(id) {
-                        if (id.includes('node_modules')) {
-                            return 'vendor';
-                        }
-                    },
-                    entryFileNames: 'commonHelpers.js',
-                },
-            },
-            outDir: '../dist',
-        },
-        plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-    };
-});
+export default defineConfig({
+    base: '/',
+})
 
