553958662aee49c1cb32a82e4057d8c81dfeb6d0
TarasNovik <148384148+TarasNovik@users.noreply.github.com>
2026-04-11 20:02:48 +0200

    html

---
+++ src/js/menu.js
@@ -0,0 +1,7 @@
+
+function openOverlay() {
+    document.getElementById("overlay").classList.add("active");
+}
+function closeOverlay() {
+    document.getElementById("overlay").classList.remove("active");
+}
